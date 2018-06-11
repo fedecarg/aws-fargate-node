18ecf41c495d8dd89c89ccce73fde1d4b88d5d6a
fedecarg <fedecarg@gmail.com>
2018-06-12 00:45:03 +0100

    Fix path to index route

--- node-webapp-example/app.js
+++ node-webapp-example/app.js
@@ -34,7 +34,7 @@ app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'handlebars');
 
 // routes
-app.use(require('./app/controllers/index'));
+app.use(require('./app/routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
